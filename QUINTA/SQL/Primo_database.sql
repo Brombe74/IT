8a8f7f836ba12de29a873f2653a06f7cdffa5cd4
Your Name <andoniu74@gmail.com>
2019-11-12 12:43:27 +0100

    Esercizi 12/11/19

--- QUINTA/SQL/Primo_database.sql
+++ QUINTA/SQL/Primo_database.sql
@@ -5,10 +5,11 @@ use STUDENTI;
 
 create table studente 
 (
-Matricola int (10)not null primary key,
+Matricola int (10)NOT NULL,
 Nome char(10) not null,
 Cognome char(10) not null,
 Anno int(4) not null,
+primary key(Matricola),
 )engine=innodb;
 
 create table corso
@@ -17,6 +18,7 @@ id int(10) not null primary key,
 Materia char(15) not null,
 Livello int(1),
 Docente char(30),
+primary key(id),
 )engine=innodb;;
 
 create table Docente
@@ -24,6 +26,7 @@ create table Docente
 id int(10) not null primary key;
 Nome char(10),
 Cognome char(10),
+primary key(id),
 )engine=innodb;
 
 create table Scuola
@@ -33,4 +36,5 @@ Nome char(20) not null,
 N_studenti int(4)not null,
 N_docenti int(2) not null,
 N_personale int(2) not null,
+primary key(id),
 )engine=innodb;
