d04035f420b0e0c94833b3f10f9f3bc7ca99fe49
Brombe74 <andoniu74@gmail.com>
2020-01-14 12:50:26 +0100

    Esercizi 14/01/20

---
+++ QUINTA/PHP/Document_write.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style media="screen">
+
+      table
+      {
+        margin: 0 auto;
+        border-collapse: collapse;
+        background: cyan;
+      }
+      tr:nth-child(odd) td:nth-child(even), tr:nth-child(even) td:nth-child(odd)
+      {
+         background: white;
+      }
+    </style>
+
+  </head>
+<body>
+
+  <h1>Output con document write</h1>
+  <h4>Sequenz adi fibonacci di 5</h4>
+
+  <?php
+    $fib=5;
+    $sequenza= array($fib);
+
+    for ($i=0; $i < $fib ; $i++)
+    {
+      $sequenza[$i]=$i+($i-1);
+    }
+
+  for($i=0;i<$fib;$i++)
+  {
+    echo "$sequenza[$i]";
+  }
+  ?>
+
+  <h4>Tavola Pitagorica di 5</h4>
+
+  <?php
+  {
+    $n_pit=5;
+
+    echo "<table border=\"1\">";
+    for ($riga=1; $riga<$n_pit+1;$riga++)
+    {
+      echo "<tr>";
+
+      for ($colonna=1; $colonna<$n_pit+1;$colonna++)
+        echo "<td>" . $riga*$colonna . "</td>";
+
+      echo "</tr>";
+    }
+    echo "</table>";
+  }
+  ?>
+
+
+</body>
+</html>
---
+++ QUINTA/PHP/ReadMe.txt
@@ -0,0 +1,12 @@
+fare sftp s01525@contabile.fermi.intra #password
+lpwd
+lcd #(se serve per spostartsi in locale)
+
+pwd
+
+cd /home/local/nomeutente/public.html #(se serve per spostarsi sul server)
+put nomefile.php .
+
+
+Dopo aver caricato i file fare
+http://contabile.fermi.intra/~s01525
