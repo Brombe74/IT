3f733d7b5a5f2aa347a41c8c5b3b4f14949b29f9
Brombe74 <andoniu74@gmail.com>
2020-01-27 09:59:00 +0100

    modifiche es 20/01/2020

--- QUINTA/PHP/form/Es1form.php
+++ QUINTA/PHP/form/Es1form.php
@@ -5,10 +5,11 @@
   </head>
   <body style="background-color:cyan;">
 
-    <form action="Riepilogo.php" method="post">
+    <form action="Riepilogo.php" method="post" style="margin-left:35%;width:50%;border:0;padding:0">
       <strong>Inserisci il Nome:</strong> <input type="text" name="nome"> <br>
       <strong>Inserisci ore lavorative</strong> <input type="text" name="ore"> <br>
       <strong>Inserisci paga per ora</strong> <input type="text" name="paga">
+      <br>
       <input type="submit">
     </form>
   </body>
