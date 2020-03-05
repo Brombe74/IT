433543cf6a1199540d99c56dc81af4d785fe84c3
Brombe74 <48101261+Brombe74@users.noreply.github.com>
2020-03-05 10:31:53 +0100

    Add files via upload

--- QUINTA/PHP/Esercitazione Esame 2013/conessione.php
+++ QUINTA/PHP/Esercitazione Esame 2013/conessione.php
@@ -7,7 +7,15 @@ $nation="";
 $id_pa="";
 $airport="";
 $motivation="";
-
+$description="";
+$genre="";
+$amount="";
+$control="";
+$id_agent="";
+$start="";
+$finish="";
+$result="";
+$payment="";
 
 
 if(isset($_POST["bottone"]))
@@ -18,6 +26,15 @@ if(isset($_POST["bottone"]))
   $id_pa=$_POST["id_p"];
   $airport=$_POST["aeroporto"];
   $motivation=$_POST["motivo"];
+  $description=$_POST["descrizione"];
+  $genre=$_POST["genere"];
+  $amount=$_POST["quantità"];
+  $control=$_POST["punto_controllo"];
+  $id_agent=$_POST["id_a"];
+  $start=$_POST["date_i"];
+  $finish=$_POST["date_f"];
+  $result=$_POST["esito"];
+  $payment=$_POST["dazio"];
 }
 
 
@@ -44,6 +61,30 @@ mysqli_select_db($connect, $database)
         echo "Record inserito <br>";
       }
 
+      $query=NULL;
+
+      $query= "CREATE TABLE IF NOT EXISTS MERCE
+      (
+        Descrizione varchar(20) NOT NULL,
+        Genere varchar(10) NOT NULL,
+        Quantità varchar(4) NOT NULL,
+        Proprietario char(10) NOT NULL,
+        PRIMARY KEY (Genere,Proprietario),
+        FOREIGN KEY (Proprietario) REFERENCES Passeggero(passaporto/identià)
+      );";
+
+      $query=NULL;
+
+      $query= "CREATE TABLE IF NOT EXISTS Controllo
+      (
+        punto_controllo
+        id_a
+        date_i
+        date_f
+        esito
+        dazio
+      );";
+
       $query= "INSERT INTO `Passeggero`(`Nome`,`Cognome`,`Nazionalita`, `Numero passaporto/identià`, `Aeroporto di provenienza`, `Motivo viaggio`)
       VALUES('$Name','$Surname','$nation','$id_pa','$airport','$motivation');";
 
@@ -56,6 +97,23 @@ mysqli_select_db($connect, $database)
       {
         echo "Error: <br>".$connect->error;
       }
+
+      $query=NULL;
+
+      $query= "INSERT INTO `MERCE`(`Descrizione`,`Genere`,`Quantità`, `Proprietario`)
+      VALUES('$description','$genre','$amount','$id_pa');";
+
+      if($connect->query($query)===TRUE)
+      {
+        echo "Record inserito <br>";
+      }
+
+      else
+      {
+        echo "Error: <br>".$connect->error;
+      }
+
+      $query=NULL;
 
       $query="SELECT * FROM Passeggero WHERE Nome='Daniel';";
       $query_select="SELECT * FROM Passeggero ORDER BY `Cognome`;"
